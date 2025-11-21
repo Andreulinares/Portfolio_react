230a52413720e4a48d061c05794abae0f23e14c5
Andreu <andreulinaresfp@ibf.cat>
2025-11-21 19:16:52 +0100

    seccion sobre mi y nav

--- src/App.js
+++ src/App.js
@@ -4,7 +4,47 @@ import foto from './Img/prueba_card.jpg';
 
 function App() {
   return (
-    <><div className="App">
+    <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Andreu Linares
+        </a>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
+        aria-label="Toggle navigation">
+          <span className="navbar-toggle-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#inicio">
+                Inicio
+              </a>
+          </li>
+          <li className="nav-item">
+              <a className="nav-link" href="#sobre-mi">
+                Sobre mí
+              </a>
+          </li>
+          <li className="nav-item">
+              <a className="nav-link" href="#proyectos">
+                Mis Proyectos
+              </a>
+          </li>
+          <li className="nav-item">
+              <a className="nav-link" href="#contacto">
+                Contacto
+              </a>
+          </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,32 +59,54 @@ function App() {
           Learn React
         </a>
       </header>
-      <h1>Portfolio</h1>
 
     </div>
-    <section className="container mt-5 text-center sobre-mi">
+
+    <section className="sobre-mi">
       <h2 className='mb-3'>Sobre mi</h2>
       <p>
         Hola, soy Andreu Linares, Desarrollador FullStack con una gran pasion por la informatica.
       </p>
     </section>
+
     <div className="container mt-5">
-        <div className="card" style={{ width: '18rem' }}>
-          <img
-            src={foto}
-            className="card-img-top"
-            alt="Ejemplo" />
-          <div className="card-body">
-            <h5 className="card-title">Proyecto 1</h5>
-            <p className="card-text">
-              Ejemplo de proyecto realizado por mi.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Ver más
-            </a>
-          </div>
+      <div className="row justify-content-center">
+
+    <div className="col-md-4 d-flex justify-content-center">
+      <div className="card mi-card" style={{ width: '18rem' }}>
+        <img src={foto} className="card-img-top" alt="Ejemplo" />
+        <div className="card-body">
+          <h5 className="card-title">Proyecto 1</h5>
+          <p className="card-text">Ejemplo de proyecto realizado por mi.</p>
+          <a href="#" className="btn btn-primary">Ver más</a>
         </div>
-      </div></>
+      </div>
+    </div>
+
+       <div className="col-md-4 d-flex justify-content-center">
+      <div className="card mi-card" style={{ width: '18rem' }}>
+        <img src={foto} className="card-img-top" alt="Ejemplo" />
+        <div className="card-body">
+          <h5 className="card-title">Proyecto 2</h5>
+          <p className="card-text">Segundo proyecto interesante.</p>
+          <a href="#" className="btn btn-primary">Ver más</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-md-4 d-flex justify-content-center">
+      <div className="card mi-card" style={{ width: '18rem' }}>
+        <img src={foto} className="card-img-top" alt="Ejemplo" />
+        <div className="card-body">
+          <h5 className="card-title">Proyecto 3</h5>
+          <p className="card-text">Tercer proyecto en el portafolio.</p>
+          <a href="#" className="btn btn-primary">Ver más</a>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    </>
   );
 }
 
